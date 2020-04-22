117d6740ad54d74ca796e489e08614f6245ce3ae
estherkimyunjung <estherkimyunjung@gmail.com>
2020-04-21 20:40:53 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,14 @@
 
 class Book
   
-  attr_accessor :title, :author, :page_count, :genre
+  attr_accessor :author, :page_count, :genre
   
   def initialize(title)
     @title = title
   end
   
+  attr_reader :title
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
